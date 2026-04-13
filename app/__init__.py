3a0d093e601af28e35c243c500c5116bee3f910e
Ranajit Mitra <ranajitmitra17@gmail.com>
2026-04-13 16:33:04 +0530

    Plan Changed: As per the plan server deleted, all things run on forntend. Simply it planned to make a serverless Tic-Tac-Toe game.

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from app.routes.game import game_bp
-    app.register_blueprint(game_bp)
-    return app
--- app/routes/game.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-game_bp = Blueprint("game", __name__)
-
-@game_bp.route("/")
-def home():
-    return render_template("board.html")
